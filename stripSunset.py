672b16751841f690a61ecbac3e196a1437943d1b
sior <matthew@sanityisoverrated.net>
2014-08-04 15:43:39 -0400

    added logging for debugging

--- stripSunset.py
+++ stripSunset.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python2
 import smbus
+import time
 bus = smbus.SMBus(1)
 bus.write_byte(0x40, 0x06)
+logFile = open("/var/log/strip.log", "a")
+logFile.write(time.strftime("%d/%m/%Y %H:%M:%S strip sunset\n"))
+logFile.close()
