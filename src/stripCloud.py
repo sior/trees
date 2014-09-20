f8f9e36064a40624f3898be75625d3fa6670d67b
sior <matthew@sanityisoverrated.net>
2014-09-19 22:56:56 -0400

    changed log date format to month/day/year

--- src/stripCloud.py
+++ src/stripCloud.py
@@ -4,6 +4,6 @@ import time
 bus = smbus.SMBus(1)
 bus.write_byte(0x40, 0x08)
 logFile = open("/var/log/strip.log", "a")
-logFile.write(time.strftime("%d/%m/%Y %H:%M:%S strip cloud\n"))
+logFile.write(time.strftime("%m/%d/%Y %H:%M:%S strip cloud\n"))
 logFile.close()
 
--- src/stripSunriseDemo.py
+++ src/stripSunriseDemo.py
@@ -4,5 +4,5 @@ import time
 bus = smbus.SMBus(1)
 bus.write_byte(0x40, 0x05)
 logFile = open("/var/log/strip.log", "a")
-logFile.write(time.strftime("%d/%m/%Y %H:%M:%S strip sunriseDemo\n"))
+logFile.write(time.strftime("%m/%d/%Y %H:%M:%S strip sunriseDemo\n"))
 logFile.close()
