acb3fdd1d8a390758e1b60f66724df6af29e19b9
sior <matthew@sanityisoverrated.net>
2014-10-02 22:26:46 -0400

    updated display of alarm time

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
 var alarmHour;
 var alarmMinute;
 var alarmAMPM;
+var alarmStatus;
+function getAlarmStatus(){
+    $.get("getAlarmStatus", function (data, status){
+        data = data.split(":");
+        alarmHour = data[0];
+        alarmMinute = data[1];
+        alarmAMPM = data[2];
+        alarmStatus = data[3];
+        
+        $("#alarmHourInput").spinner("value", parseInt(alarmHour));
+        $("#alarmHourSlider").slider("value", parseInt(alarmHour));
+        $("#alarmMinuteInput").spinner("value", parseInt(alarmMinute));
+        $("#alarmMinuteSlider").slider("value", parseInt(alarmMinute));
+        
+        if (alarmAMPM == "am"){
+            $("#AMButton").attr("checked", "checked").button("refresh");
+            $("#PMButton").attr("checked", "unchecked").button("refresh");
+        } else {
+            $("#PMButton").attr("checked", "checked").button("refresh");
+            $("#AMButton").attr("checked", "unchecked").button("refresh");
+        }
+
+        if (alarmStatus == "on"){
+            $("#alarmButton").children().text("Turn Alarm Off");
+        } else {
+            $("#alarmButton").children().text("Turn Alarm On");
+        }
+
+        updateAlarmStatusString();
+    });
+}
 
 var buttonWidth = 0;
 function getWidth(){
@@ -13,11 +44,32 @@ function getWidth(){
 function updateHourSlider(){
     var hour = $("#alarmHourInput").spinner("value");
     $("#alarmHourSlider").slider("value", hour);
+    alarmHour = hour.toString();
+    updateAlarmStatusString();
 }
 
 function updateMinuteSlider(){
     var minute = $("#alarmMinuteInput").spinner("value");
     $("#alarmMinuteSlider").slider("value", minute);
+    if (minute.toString().length == 1){
+        alarmMinute = "0" + minute.toString();
+    } else {
+        alarmMinute = minute.toString();
+    }
+    updateAlarmStatusString();
+}
+
+function updateAlarmStatusString(){
+    var statusString;
+    if (alarmStatus == "on"){
+        statusString = "Alarm set for ";
+        statusString += alarmHour + ":";
+        statusString += alarmMinute + " ";
+        statusString += alarmAMPM;
+    } else {
+        statusString = "Alarm Off";
+    }
+    $("#alarmHeader").text(statusString);
 }
 
 $(document).ready(function() {
@@ -30,13 +82,14 @@ $(document).ready(function() {
 
     $("#alarmButton").button().click(function() {
         var alarmButton = $("#alarmButton");
-        if (alarmButton.text() == "Alarm On"){
-            alarmButton.children().text("Alarm Off");
-            $("#alarmHeader").text("Alarm Is On");
+        if (alarmButton.text() == "Turn Alarm On"){
+            alarmButton.children().text("Turn Alarm Off");
+            alarmStatus = "on";
         } else {
-            alarmButton.children().text("Alarm On");
-            $("#alarmHeader").text("Alarm Is Off");
+            alarmButton.children().text("Turn Alarm On");
+            alarmStatus = "off";
         }
+        updateAlarmStatusString();
     });
 
     $("#alarmHourInput").spinner({
@@ -62,6 +115,8 @@ $(document).ready(function() {
         value:6,
         slide: function (event, ui){
             $("#alarmHourInput").val(ui.value);
+            alarmHour = ui.value.toString();
+            updateAlarmStatusString();
         }
     }).position({
         my:"center top", 
@@ -76,6 +131,12 @@ $(document).ready(function() {
         value:30,
         slide: function (event, ui){
             $("#alarmMinuteInput").val(ui.value);
+            if (ui.value.toString().length == 1){
+                alarmMinute = "0" + ui.value.toString();
+            } else {
+                alarmMinute = ui.value.toString();
+            }
+            updateAlarmStatusString();
         }
     }).position({
         my:"center top", 
@@ -89,10 +150,22 @@ $(document).ready(function() {
         at:"center+75 center",
         of:"#alarmMinuteSlider"
     });
+
+    $("#AMButton").click(function (){
+        alarmAMPM = "am";
+        updateAlarmStatusString();
+    });
+
+    $("#PMButton").click(function (){
+        alarmAMPM = "pm";
+        updateAlarmStatusString();
+    });
+
     var buttons = $("#buttons").children();
     buttons.each(getWidth);
     buttons.width(buttonWidth + 5);
     buttons.css('margin-top', '2px');
     buttons.css('margin-bottom', '2px');
     $("#alarmContent").height($("#alarmContent").height() + 25);
+    getAlarmStatus();
 });
