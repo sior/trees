dbe564c648ce1a7d994c70456740095e504c7f1b
sior <matthew@sanityisoverrated.net>
2014-11-20 12:57:19 -0500

    Having IOErrors when trying to send commands to the Trinket.  Added error handleing to try and work around this.

--- src/stripCloud.py
+++ src/stripCloud.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python2
 import smbus
 import time
-bus = smbus.SMBus(1)
-bus.write_byte(0x40, 0x08)
+done = False
+while not done:
+    try:
+        bus = smbus.SMBus(1)
+        bus.write_byte(0x40, 0x08)
+        done = True
+    except IOError as e:
+        done = False
+        time.sleep(0.5)
 logFile = open("/var/log/strip.log", "a")
 logFile.write(time.strftime("%m/%d/%Y %H:%M:%S strip cloud\n"))
 logFile.close()
--- src/stripSunsetDemo.py
+++ src/stripSunsetDemo.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python2
 import smbus
 import time
-bus = smbus.SMBus(1)
-bus.write_byte(0x40, 0x07)
+done = False
+while not done:
+    try:
+        bus = smbus.SMBus(1)
+        bus.write_byte(0x40, 0x07)
+        done = True
+    except IOError as e:
+        done = False
+        time.sleep(0.5)
 logFile = open("/var/log/strip.log", "a")
 logFile.write(time.strftime("%d/%m/%Y %H:%M:%S strip sunsetDemo\n"))
 logFile.close()
